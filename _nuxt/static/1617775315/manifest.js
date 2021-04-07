758c345afecf6927772c43b9546c0e0621972313
Risolat <risolat.utkurova@gmail.com>
2021-04-07 11:28:45 +0500

    Updates

--- _nuxt/static/1617775315/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002FmurojaatYuborish","\u002F","\u002Fmurojaat","\u002Fbatafsil"]})
---
+++ _nuxt/static/1617776914/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fmurojaat","\u002F","\u002Fbatafsil","\u002FmurojaatYuborish"]})
