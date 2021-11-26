e43e27ce63a4844df9f68287bf94097a14d410ac
Risolat <risolat.utkurova@gmail.com>
2021-11-26 16:40:12 +0500

    change login page

--- pages/login.vue
+++ pages/login.vue
@@ -2,7 +2,9 @@
   <div class="container">
     <div class="login-form">
       <p>{{$t('logo.signIn')}}</p>
-     <b-form>
+
+      <p @click="postOneId()" class="login-link">OneId orqali kirish</p>
+     <!-- <b-form>
       <b-form-group id="input-group-1" :label="$t('auth.phone')" label-for="input-1">
         <div class="phone-wrapper">
         <span>+998</span>
@@ -27,11 +29,11 @@
       </b-form-group>
       <nuxt-link :to="localePath('/forgetPassword')">Forget Password?</nuxt-link>
       <button @click="signIn()" type="button">{{$t('auth.text')}}</button>
-     </b-form>
+     </b-form> -->
      </div>
-    <div class="reg-box">
+    <!-- <div class="reg-box">
       <nuxt-link :to="localePath('/reg')" >{{$t('auth.reg')}}</nuxt-link>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -42,6 +44,13 @@ export default {
       form: {
         phone: "",
         password: ""
+      },
+      formData: {
+        response_type: 'one_code',
+        client_id: 'gis_xn_mobile',
+        redirect_uri: 'https://xn.technocorp.uz/api/one-id/432fdsfHHDSFG213__LL-123QWE',
+        scope: 'gis_xn_mobile',
+        state: 'b"™ëa¡Ò\x03="'
       }
     };
   },
@@ -54,9 +63,30 @@ export default {
       } catch (err) {
         console.log(err);
       }
+    },
+    async postOneId(){
+      await this.$axios.post(`https://sso.gov.uz:8443/sso/oauth/Authorization.do`, this.formData )
+        .then(res => {
+          console.log(res);
+        })
+        .catch(err => {
+          console.log(err);
+        })
     }
   }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.login-link{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  color: #2a97fc;
+}
+.login-link:hover{
+  text-decoration: underline;
+  transition: all ease .3s;
+}
+</style>
